062027b2df1cc733191f4855034c509028b4267e
Maicol <michael.negrete16@gmail.com>
2022-07-06 17:16:51 -0500

    Variables de entorno + Carpeta publica

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express')
+// Libreria para poder utilizar las variables de entorno (npm i dotenv)
+require('dotenv').config()
 
 // Crear servidor de express
 const app = express()
 
+// Directorio publico
+app.use(express.static('public'))
+
 // Rutas de la aplicacion para dar respuestas a la ruta ('/')
-app.get('/', (req,res) => {
-    console.log('Se requiere el /')
-    res.json({
-        ok:true
-    })
-})
+// app.get('/', (req,res) => {
+//     console.log('Se requiere el /')
+//     res.json({
+//         ok:true
+//     })
+// })
 
 // Escuchar peticiones en el puerto 4000
-app.listen(4000, () => {
-    console.log(`Servidor corriendo en puerto ${4000}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })
