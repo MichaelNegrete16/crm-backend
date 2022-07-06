13dc24794c9f7e83d4e032f81687dfb2395d0392
Maicol <michael.negrete16@gmail.com>
2022-07-06 17:06:50 -0500

    configuracion de express + escuchar peticiones

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hola mundo!!')
+const express = require('express')
+
+// Crear servidor de express
+const app = express()
+
+// Rutas de la aplicacion para dar respuestas a la ruta ('/')
+app.get('/', (req,res) => {
+    console.log('Se requiere el /')
+    res.json({
+        ok:true
+    })
+})
+
+// Escuchar peticiones en el puerto 4000
+app.listen(4000, () => {
+    console.log(`Servidor corriendo en puerto ${4000}`)
+})
